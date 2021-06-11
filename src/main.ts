2c9693b1ec1c5636af9b69783cbc2c7695458eb2
chfaraz <ch.faraz324@gmail.com>
2021-06-11 17:07:30 +0500

    logs added

--- src/main.ts
+++ src/main.ts
@@ -7,5 +7,13 @@ async function bootstrap() {
   const port = process.env.PORT || 3000;
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(port);
+
+  console.log(process.env.NODE_ENV);
+  console.log(process.env.DB_PORT);
+  console.log(process.env.DB_NAME);
+  console.log(process.env.DB_HOST);
+  console.log(process.env.DB_USER_NAME);
+  console.log(process.env.DB_PASSWORD);
+  console.log(process.env.JWT_SECRET);
 }
 bootstrap();
