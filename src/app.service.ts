3dab444b97a19fab9cd2e5786aacbaab9f1d3f20
ch.faraz <ch.faraz324@gmail.com>
2021-06-14 15:29:55 +0500

    production branch added

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return '====>> server is up and running';
+    return '===================================>> PRODUCTION server is up and running <<===================================';
   }
 }
